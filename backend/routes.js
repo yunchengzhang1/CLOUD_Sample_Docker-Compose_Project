8c1865add22fb3d97e2d8fc78da6ed34e971ddaa
BrandonHerman <brandonherman0202@gmail.com>
2021-12-02 13:22:02 -0600

    sum num likes get statement

--- backend/routes.js
+++ backend/routes.js
@@ -516,3 +516,30 @@ app.put('/likemessage', (req, res) => {
 }
 )}
 
+app.get('/getbattlescore', function (req, res) {
+
+
+  var battleID = req.param('battleID');
+  var userID = req.param('userID');
+
+  // obtain a connection from our pool of connections
+  pool.getConnection(function (err, connection) {
+    if (err) {
+      // if there is an issue obtaining a connection, release the connection instance and log the error
+      logger.error('Problem obtaining MySQL connection', err)
+      res.status(400).send('Problem obtaining MySQL connection');
+    } else {
+      // if there is no issue obtaining a connection, execute query and release connection
+      connection.query('SELECT SUM(`numLikes) FROM `db`.`messages` WHERE `battleID`=? AND `userID`=?', [battleID, userID], function (error, results, fields) {
+        connection.release();
+        if (err) {
+          // if there is an error with the query, log the error
+          logger.error("Problem getting from test table: \n", err);
+          res.status(400).send('Problem getting from table');
+        } else {
+          res.end(JSON.stringify(results));
+        }
+      });
+    }
+  });
+});
